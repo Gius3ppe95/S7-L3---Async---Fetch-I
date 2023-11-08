0995122c233e9c71d26091b1155b6143f572337d
Gius3ppe95 <Snak3Ey3s.2023@gmail.com>
2023-11-08 16:47:55 +0100

    commit 2- library

--- books.js
+++ books.js
@@ -1,9 +1,72 @@
 const cart = [];
 
-fetch("https://striveschool-api.herokuapp.com/books").then((response) => {
-  if (response.ok) {
-    return response.json();
-  } else {
-    throw new Error("Errore");
+fetch("https://striveschool-api.herokuapp.com/books")
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("Errore");
+    }
+  })
+  .then((data) => {
+    const container = document.getElementById("book-container");
+    data.forEach((book) => {
+      const newCol = document.createElement("div");
+      newCol.classList.add("col-4");
+      newCol.classList.add("col-md-3");
+      const newBook = document.createElement("div");
+      newBook.classList.add("card");
+
+      newBook.innerHTML = new Card(book.img, book.title, book.price, book.asin).toString();
+      newCol.appendChild(newBook);
+      container.appendChild(newCol);
+
+      document.getElementById(`discard-${book.asin}`).addEventListener("click", function (e) {
+        e.target.parentElement.parentElement.parentElement.style.display = "none";
+      });
+
+      document.getElementById(`buy-${book.asin}`).addEventListener("click", function (e) {
+        if (!containsBook(cart, book.asin)) {
+          cart.push(book);
+          refreshCart();
+        }
+      });
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+function containsBook(arr, asin) {
+  let res = false;
+  arr.forEach((e) => {
+    if (e.asin === asin) {
+      res = true;
+    }
+  });
+  return res;
+}
+
+class Card {
+  constructor(image, title, price, asin) {
+    this.image = image;
+    this.title = title;
+    this.price = price;
+    this.asin = asin;
   }
-});
+  toString() {
+    return `
+    
+    <img src="${this.image}" class="card-img-top" alt="" />
+    <div class="card-body d-flex flex-column">
+      <h5 class="card-title fs-6">${this.title}</h5>
+      <p class="card-text flex-grow-1">
+        Prezzo: ${this.price} &euro;
+      </p>
+      <a href="#" class="btn btn-primary" id="discard-${this.asin}">Scarta</a>
+      <a href="#" class="btn btn-info mt-2" id="buy-${this.asin}">Compra Ora</a>
+    </div>
+  
+`;
+  }
+}
